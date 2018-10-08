97faa35f45b46d61d6a1a56d5d1b6d8c4fcbc6d1
Roman Kreuzhuber <rkreuzh@gmail.com>
2018-10-08 16:50:50 +0100

    fix minor bug, enable example intervals file for 1kb sequences

--- kipoiseq/datasets/sequence.py
+++ kipoiseq/datasets/sequence.py
@@ -293,7 +293,6 @@ class SeqDataset(Dataset):
             doc: None, datatype of the task labels taken from the intervals_file. Allowed - string', 'int', 'float', 'bool'
         auto_resize_len:
             doc: None, required sequence length.
-            example: 3
         use_strand:
             doc: reverse-complement fasta sequence if bed file defines negative strand
         alphabet_axis:
@@ -348,7 +347,7 @@ class SeqDataset(Dataset):
 
         # how to transform the input
         self.input_tranform = Compose([
-            OneHot(self.alphabet),  # one-hot-encode
+            OneHot(self.alphabet, dtype=self.dtype),  # one-hot-encode
             DummyAxis(self.dummy_axis),  # optionally inject the dummy axis
             SwapAxes(1, self.alphabet_axis),  # put the alphabet axis elsewhere
         ])
