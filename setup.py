a3bca52adea186f9a229ae7378b0026b73cd9604
Muhammed Hasan Celik <muhammedhasancelik@gmail.com>
2020-01-23 18:02:54 +0100

    test dep

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ test_requirements = [
     "pytest>=3.3.1",
     "pytest-xdist",  # running tests in parallel
     "pytest-pep8",  # see https://github.com/kipoi/kipoi/issues/91
+    "pytest-mock",
     "pytest-cov",
     "coveralls",
     "scikit-learn",
