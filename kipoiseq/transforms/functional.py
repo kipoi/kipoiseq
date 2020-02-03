77018d9b7bc15e572e72633ae62c2df10135dfea
Kalin Nonchev <nonchev@ouga04.i12g.informatik.tu-muenchen.de>
2020-02-03 14:25:59 +0100

    U acid added

--- kipoiseq/transforms/functional.py
+++ kipoiseq/transforms/functional.py
@@ -264,7 +264,7 @@ TRANSLATION_TABLE = {
     'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
     'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
     'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
-    'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
+    'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': 'U',
     'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W',
 }
 
