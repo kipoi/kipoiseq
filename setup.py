f12f6254bb973806454de50db02ad85270d1e469
Muhammed Hasan Celik <muhammedhasancelik@gmail.com>
2020-03-02 18:35:18 +0100

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ test_requirements = [
 
 setup(
     name='kipoiseq',
-    version='0.3.3',
+    version='0.3.4',
     description="kipoiseq: sequence-based data-laoders for Kipoi",
     author="Kipoi team",
     author_email='avsec@in.tum.de',
