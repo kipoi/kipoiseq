66283454017e8508283645ccfc5cb883e95633a6
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2021-10-23 15:17:48 +0200

    Bump version: 0.6.0 → 0.7.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ test_requirements = [
 
 setup(
     name='kipoiseq',
-    version='0.6.0',
+    version='0.7.0',
     description="kipoiseq: sequence-based data-loaders for Kipoi",
     author="Kipoi team",
     author_email='avsec@in.tum.de',
